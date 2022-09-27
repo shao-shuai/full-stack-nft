b728c4d608fd96e5d9b65aaaaeb8d22c3cb4a79b
shao-shuai <shuai-shao@outlook.com>
2022-09-26 22:53:34 -0700

    update UI

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Buton, Flex, Image, Link, Spacer } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Link, Spacer } from "@chakra-ui/react";
 import Facebook from "./assets/social-media-icons/facebook_32x32.png";
 import Twitter from "./assets/social-media-icons/twitter_32x32.png";
 import Email from "./assets/social-media-icons/email_32x32.png";
@@ -19,26 +19,39 @@ const NavBar = ({ accounts, setAccounts }) => {
   return (
     <Flex justify="space-between" align="center" padding="30px">
       <Flex justify="space-around" width="40%" padding="0 75px">
-        <Link href="https:www.facebook.com">
+        <Link href="https://www.facebook.com">
           <Image src={Facebook} boxSize="42px" margin="0 15px"/>
         </Link>
-        <Link href="https:www.facebook.com">
+        <Link href="https://www.twitter.com">
           <Image src={Twitter} boxSize="42px" margin="0 15px"/>
         </Link>
-        <Link href="https:www.facebook.com">
+        <Link href="https://www.gmail.com">
           <Image src={Email} boxSize="42px" margin="0 15px"/>
         </Link>
       </Flex>
-      <div>Twitter</div>
-      <div>Email</div>
-      <div>Abount</div>
-      <div>Mint</div>
-      <div>Team</div>
+      <Flex justify="space-around" align="center" width="40px" padding="30px">
+      <Box margin="0 15px">About</Box>
+      <Spacer />
+      <Box margin="0 15px">Mint</Box>
+      <Spacer />
+      <Box margin="0 15px">Team</Box>
+      <Spacer />
       {isConnected ? (
-        <p>Connected</p>
+        <Box margin="0 15px">Connected</Box>
       ) : (
-        <button onClick={connectAccount}>Connect</button>
+        <Button
+        backgroundColor="#D6517D"
+        borderRadius="5px"
+        boxShadow="0px 2px 2px 1px #OFOFOF"
+        color="white"
+        cursor="pointer"
+        fontFamily="inherit"
+        padding="15px"
+        margin="0 15px"
+         onClick={connectAccount}>Connect</Button>
       )}
+      </Flex>
+      
     </Flex>
   );
 };
